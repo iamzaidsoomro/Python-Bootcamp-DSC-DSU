2b658a908ddfb2afaccb86942687161fd4e94966
Zaid Ahmed <75375280+iamzaidsoomro@users.noreply.github.com>
2020-12-10 14:00:05 +0500

    NameAssignment.py
    python

---
+++ week_1/NameAssignment.py
@@ -0,0 +1,15 @@
+# Instructors: Tarun Kumar & Bahawal Baloch
+# Developer: Zaid Ahmed Soomro
+# Institute: MUET Jamshoro
+# Department: Software Engineering (First Semester)
+# Language: Python
+# DSU-DSC Python Bootcamp 2020
+# Date: 9/12/2020 Wednesday
+
+def foo():
+    name = ["Z", "  A", "    I", "      D"]
+    for i in name:
+        print(i)
+
+
+foo()
